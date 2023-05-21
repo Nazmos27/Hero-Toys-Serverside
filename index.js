a0cfb01b05217838a580fc8f39c7f5b7a57d637d
Nazmos27 <nazmos789@gmail.com>
2023-05-21 20:25:43 +0600

    delete operation implemented

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/',(req,res) => {
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.choi6e7.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -63,6 +63,13 @@ async function run() {
             query = {email : req.query.email}
         }
         const result = await addedToysCollection.find(query).toArray()
+        res.send(result)
+    })
+
+    app.delete('/addtoys/:id', async(req,res) => {
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result =await addedToysCollection.deleteOne(query)
         res.send(result)
     })
 
